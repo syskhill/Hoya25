a6ead39cffa5db0c2a42462879cd2c5f9908ecc8
Visnu S Pandian <visnupandian1@gmail.com>
2025-01-24 22:45:13 -0500

    Created python app file with objectives.

---
+++ app.py
@@ -0,0 +1,13 @@
+# Developing a python application
+# File Metadata Analyzer
+
+# File type
+# File size
+# Binary hash(?)
+# Type changes
+# Timestamps
+# Descrepencies between metadata and file data
+# Corrupted files
+# Past corruption(?)
+# Last access/damage
+# Specific data based on filetype
