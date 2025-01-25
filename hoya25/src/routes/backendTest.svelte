7076dd342105933c09b99f941635abbf55106ff6
Connor Cahill <connorcahill06@gmail.com>
2025-01-25 02:04:21 -0500

    Actually added API testing

---
+++ hoya25/src/routes/backendTest.svelte
@@ -0,0 +1,9 @@
+<script>
+    async function fetchData() {
+      const response = await fetch('/api/data');
+      const data = await response.json();
+      console.log(data);
+    }
+  
+    fetchData();
+</script>
