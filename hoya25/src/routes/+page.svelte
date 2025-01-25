b24cff93d7e49a8c378bc6a9befae33c93cde700
alanachen777 <alana.chen777@gmail.com>
2025-01-25 02:00:01 -0500

    enhanced frontend?

--- hoya25/src/routes/+page.svelte
+++ hoya25/src/routes/+page.svelte
@@ -1,3 +1,44 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import DropIn from './dropIn.svelte';
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        border: none;
+        outline: none;
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+    .navbar {
+        background-color: #333;
+        color: white;
+        padding: 1rem;
+        text-align: center;
+    }
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        flex-direction: column;
+    }
+</style>
+
+<div class="navbar">
+    <h1> insert logo-name </h1>
+</div>
+
+<div class="container">
+    <h2>Purpose</h2>
+    <p>
+        insert the website purpose lol
+    </p>
+    
+    <DropIn />
+
+    <button class="button">Upload File</button>
+    <button class="button">Check Your File</button>
+</div>
